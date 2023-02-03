52cac2552bb990d3a04c65a214be1ca0cc783285
Divyanshureco <124235011+Divyanshureco@users.noreply.github.com>
2023-02-03 11:58:59 +0530

    Add files via upload

---
+++ list_python.py
@@ -0,0 +1,30 @@
+# create a list of prime numbers
+prime_numbers = [2, 3, 5, 7]
+
+# reverse the order of list elements
+prime_numbers.reverse()
+
+
+print('Reversed List:', prime_numbers)
+
+# Output: Reversed List: [7, 5, 3, 2]
+
+prime_numbers = [11, 3, 7, 5, 2]
+
+# sorting the list in ascending order
+prime_numbers.sort()
+
+print(prime_numbers)
+
+# Output: [2, 3, 5, 7, 11] 
+
+# mixed list
+prime_numbers = [2, 3, 5]
+
+# copying a list
+numbers = prime_numbers.copy()
+
+
+print('Copied List:', numbers)
+
+# Output: Copied List: [2, 3, 5]
